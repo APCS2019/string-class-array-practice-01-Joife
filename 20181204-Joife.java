3ca22b26794e3a0e7198cd3df3b30de57e231d5b
Joife <45375723+Joife@users.noreply.github.com>
2018-12-04 09:45:21 +0800

    Update 20181204-Joife.java

--- 20181204-Joife.java
+++ 20181204-Joife.java
@@ -1,3 +1,24 @@
 Past paper 2015
 main idea: String, class
 public class HiddenWord {
+  private string puzzle;
+  public HiddenWord(String hidddenWord) {
+    this.puzzle = hiddenWord;
+    }
+  public String getHint(String guess) {
+    String temp = "";
+    for (int i = 0; i < guess.length(); i++) {
+      if (puzzle.subString(i,i+1).equals(guess.subString(i,i+1)) {
+        temp += puzzle.subString(i,i+1);
+        }
+      else if(puzzle.indexOf(guess.substring((i,i+1) != -1) {
+        temp += "+";
+        }
+      else {
+        temp += "*";
+        }
+      }
+       return temp;
+    }
+  }
+ }
